063793979567e55f77039f2fce57604381d51d93
rbagriy-nix <rbagriy@n-ix.com>
2021-02-25 15:47:12 +0200

    created Home component, added router, created empty components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from "./components/todo/todo.component";
+import { HomeComponent } from "./components/home/home.component";
+import {ShopComponent} from "./components/shop/shop.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'todo', component: TodoComponent },
+  { path: 'shop', component: ShopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
