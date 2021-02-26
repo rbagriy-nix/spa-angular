939783b53417a2b0cd68ec0dabf7eecae42c1096
rbagriy-nix <rbagriy@n-ix.com>
2021-02-26 11:05:46 +0200

    added shop component

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from './data'
 
 @Component({
   selector: 'app-shop',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+
+  products = data
 
   constructor() { }
 
