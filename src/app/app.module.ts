be78225976576cc05e01535b377f5b469fce7e1e
rbagriy-nix <rbagriy@n-ix.com>
2021-02-26 09:45:51 +0200

    added todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
 import {CdkStepperModule} from '@angular/cdk/stepper';
 import {CdkTableModule} from '@angular/cdk/table';
 import {CdkTreeModule} from '@angular/cdk/tree';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {OverlayModule} from '@angular/cdk/overlay';
+
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 import {MatButtonModule} from '@angular/material/button';
@@ -20,17 +21,16 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatCardModule} from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatTreeModule} from '@angular/material/tree';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -41,12 +41,15 @@ import {MatSliderModule} from '@angular/material/slider';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatSortModule} from '@angular/material/sort';
+import {MatStepperModule} from '@angular/material/stepper';
 import {MatTableModule} from '@angular/material/table';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
-import {OverlayModule} from '@angular/cdk/overlay';
+import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+
+
 import { TodoComponent } from './components/todo/todo.component';
 import { HomeComponent } from './components/home/home.component';
 import { ShopComponent } from './components/shop/shop.component';
@@ -59,23 +62,12 @@ import { ShopComponent } from './components/shop/shop.component';
     ShopComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    A11yModule,
-    ClipboardModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,
     MatAutocompleteModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
     MatCheckboxModule,
     MatChipsModule,
-    MatStepperModule,
     MatDatepickerModule,
     MatDialogModule,
     MatDividerModule,
@@ -85,7 +77,6 @@ import { ShopComponent } from './components/shop/shop.component';
     MatInputModule,
     MatListModule,
     MatMenuModule,
-    MatNativeDateModule,
     MatPaginatorModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
@@ -97,14 +88,26 @@ import { ShopComponent } from './components/shop/shop.component';
     MatSlideToggleModule,
     MatSnackBarModule,
     MatSortModule,
+    MatStepperModule,
     MatTableModule,
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    MatTreeModule,
-    OverlayModule,
+    AppRoutingModule,
+    BrowserModule,
+    A11yModule,
+    ClipboardModule,
+    DragDropModule,
     PortalModule,
     ScrollingModule,
+    CdkStepperModule,
+    CdkTableModule,
+    MatBadgeModule,
+    CdkTreeModule,
+    MatBottomSheetModule,
+    OverlayModule,
+    MatTreeModule,
+    MatNativeDateModule
   ],
   providers: [{
     provide: APP_BASE_HREF,
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  selector: "todo-component",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.scss"]
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent implements OnInit{
+  public tasks: Array<{ text: string, done: boolean }>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.tasks = [];
   }
 
+  ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+  addTask(task: string) {
+    if (task === "" || task.trim() === "") return;
+    this.tasks.push({ text: task, done: false});
+  }
+
+  removeTask(task:any) {
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+  }
+
+  toggle(task:any) {
+    task.done = !task.done;
+  }
 }
